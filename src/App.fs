5c7453a0287096b62ff2e74a624353fd395cafc4
UnintelligentCode <victoraliharris@gmail.com>
2022-11-15 17:35:32 -0500

    set layout for grid

--- src/App.fs
+++ src/App.fs
@@ -10,10 +10,19 @@ mainDiv.setAttribute("id", "mainDiv")
 let sideNav = document.createElement "div" :?> HTMLDivElement
 sideNav.textContent <- "Side"
 sideNav.style.setProperty("grid-area", "snav")
+sideNav.style.backgroundColor <- "yellow"
+sideNav.style.width <- "9vw"
 let header = document.createElement "div" :?> HTMLDivElement
 header.textContent <- "Head"
+header.style.setProperty("grid-area", "head")
+header.style.backgroundColor <- "biege"
+header.style.height <- "10vh"
 let main = document.createElement "div" :?> HTMLDivElement
 main.textContent <- "Main"
+main.style.setProperty("grid-area", "main")
+main.style.backgroundColor <- "red"
+main.style.height <- "88vh"
+main.style.width <- "90vw"
 
 body.appendChild mainDiv |> ignore
 mainDiv.appendChild header |> ignore
@@ -21,3 +30,4 @@ mainDiv.appendChild sideNav |> ignore
 mainDiv.appendChild main |> ignore
 
 console.log body
+console.log window.innerHeight
